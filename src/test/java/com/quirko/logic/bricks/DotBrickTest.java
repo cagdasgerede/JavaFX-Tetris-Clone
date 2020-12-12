f1a272f76b5c9c16c2d0613393911dc44b9dee29
CanKocyigitoglu <kocyigitoglucan@gmail.com>
2020-12-12 19:28:36 +0300

    tests are added

---
+++ src/test/java/com/quirko/logic/bricks/DotBrickTest.java
@@ -0,0 +1,31 @@
+package com.quirko.logic.bricks;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DotBrickTest {
+
+    @Test
+    public void testIsShapeDot() throws Exception {
+        Brick brick = new DotBrick();
+        int brickCounter = 0;
+        boolean isBrickDotShaped = false;
+        for (int i = 0; i < brick.getShapeMatrix().get(0).length; i++) {
+            for (int j = 0; j < brick.getShapeMatrix().get(0)[i].length; j++) {
+                if (brick.getShapeMatrix().get(0)[i][j] > 0) {
+                    brickCounter += 1; 
+                }
+            } 
+        }
+        if (brickCounter == 1) {
+            isBrickDotShaped = true;
+        }
+        Assert.assertTrue(isBrickDotShaped);
+    }
+
+    @Test
+    public void testGetShapeMatrixList() throws Exception {
+        Brick brick = new DotBrick();
+        Assert.assertEquals(1, brick.getShapeMatrix().size());
+    }
+}
