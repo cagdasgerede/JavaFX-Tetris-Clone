b784050d6b52849427b7c60d9f5afed77aaa74b3
GizemAtasoy <gatasoy@etu.edu.tr>
2021-04-11 22:22:59 +0300

    son

--- src/main/java/com/quirko/app/GameController.java
+++ src/main/java/com/quirko/app/GameController.java
@@ -7,7 +7,9 @@ import com.quirko.logic.events.InputEventListener;
 import com.quirko.logic.events.MoveEvent;
 
 public class GameController implements InputEventListener {
-
+    int sayac = 0;
+    int y_sayac = 0;
+    int bff ;
     private Board board = new SimpleBoard(25, 10);
 
     private final GuiController viewGuiController;
@@ -22,20 +24,45 @@ public class GameController implements InputEventListener {
 
     @Override
     public DownData onDownEvent(MoveEvent event) {
+        System.out.println("ysayac = " + y_sayac);
+        System.out.println("sayac = "  + sayac);
+        System.out.println("bff = " + bff);
+        if(viewGuiController.buff == 1 || viewGuiController.buff == 2 || viewGuiController.buff == 3){
+            y_sayac = sayac;
+            bff = viewGuiController.buff;
+        }
+
         boolean canMove = board.moveBrickDown();
         ClearRow clearRow = null;
+
         if (!canMove) {
             board.mergeBrickToBackground();
             clearRow = board.clearRows();
-            if (clearRow.getLinesRemoved() > 0) {
+            int c = clearRow.getLinesRemoved(); 
+            System.out.println(board);
+
+            if(sayac-1 == y_sayac && bff == 1) { // row clear
+                board.getScore().add(10);
+            }
+
+            if(sayac-1 == y_sayac && bff == 2) { // bomb
+
+            }
+
+            if(sayac-1 == y_sayac && bff == 3) { // change break
+            
+            }
+
+            if (c > 0) {
                 board.getScore().add(clearRow.getScoreBonus());
             }
+
             if (board.createNewBrick()) {
                 viewGuiController.gameOver();
             }
 
             viewGuiController.refreshGameBackground(board.getBoardMatrix());
-
+            sayac++;
         } else {
             if (event.getEventSource() == EventSource.USER) {
                 board.getScore().add(1);
